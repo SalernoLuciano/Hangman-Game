164ec12fc845ca09f30db22db8aa229d65c81a99
Luciano Salerno <salerno.luciano@outlook.com.ar>
2022-02-07 20:12:40 -0300

    Basico de start y addWord hecho

--- js/juego.js
+++ js/juego.js
@@ -4,10 +4,25 @@ const inputNewWord = document.querySelector("#input-add-word");
 const canvas = document.querySelector( "canvas" );
 const brush = canvas.getContext("2d");
 
+let dictionary = [ "ACUARIO", "BIFE", "COLMENA", "DADO", "ECLIPSE", "FLEXIBLE", "GLUCEMIA", "HUMILDE", "IMPERIO", "JURAR", "LABERINTO", "MURO", "NOVATO", "OBTUSO", "PIONONO", "QUIETO", "REY", "SASTRE", "TRAGEDIA", "UPAR", "VAGABUNDO", "WINDSURF", "EXPRIMIR", "BYTE", "ZANGUANGO"];
+
 let randomWord;
 let inputLetter;
 let lives = 7;
 
-btnStart.addEventListener("click", startGame());
+btnStart.addEventListener("click", startGame);
 
-btnAddWord.addEventListener("click", addNewWord());
+btnAddWord.addEventListener("click", addNewWord);
+
+function startGame(){
+    randomWord = selectRandomWord().split("");
+}
+
+function selectRandomWord(){
+    return dictionary[ Math.floor( Math.random() * dictionary.length ) ];
+}
+
+function addNewWord(){
+    let input = inputNewWord.value;
+    console.log(input);
+}
